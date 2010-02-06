b7d45b2e325512861a0ef23e756a81bfdf3adaf7
James Aylett <james@tartarus.org>
2010-02-06 12:56:21 +0000

    Bugfixes:  * threads after the first were raising exceptions instead of defaulting to a stashed user of None  * don't overwrite created_by if it's already been set elsewhere (allowing users to proxy create objects)  * don't set modified_by to AnonymousUser (ie: require is_authenticated())

--- middleware.py
+++ middleware.py
@@ -26,15 +26,13 @@ stash = threading.local()
 
 def get_current_user():
     """Get the user whose session resulted in the current code running. (Only valid during requests.)"""
-    return stash.current_user
+    return getattr(stash, 'current_user', None)
 
 def set_current_user(user):
     stash.current_user = user
 
-set_current_user(None)
-
 def onanymodel_presave(sender, **kwargs):
-    current_user = stash.current_user
+    current_user = get_current_user()
     if current_user is None or not current_user.is_authenticated():
         # if there is no current user or we're an unauthenticated user (ie: guest)
         # then don't do anything. The save() will fail if created_by or modified_by
@@ -46,11 +44,12 @@ def onanymodel_presave(sender, **kwargs):
     if hasattr(obj, 'modified_at'):
         obj.modified_at = datetime.datetime.now()
     if hasattr(obj, 'modified_by_id'):
-        obj.modified_by = current_user
+        if current_user and current_user.is_authenticated():
+            obj.modified_by = current_user
     if not obj.pk:
         if hasattr(obj, 'created_at'):
             obj.created_at = datetime.datetime.now()
-        if hasattr(obj, 'created_by_id'):
+        if hasattr(obj, 'created_by_id') and not obj.created_by:
             obj.created_by = current_user
 
 pre_save.connect(onanymodel_presave)
